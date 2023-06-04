c8e11df31ace468ed5a4c8455ffbbb08581525a0
Brian Wells <spam_brian@me.com>
2023-06-04 15:37:28 -0400

    Change from a module to an app

--- gotest.go
+++ gotest.go
@@ -1,6 +1,17 @@
-package gotest
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, location := range os.Args[1:] {
+		message := Weather(location)
+		fmt.Println(message)
+	}
+}
+
 
 func Weather(location string) string {
 	message := fmt.Sprintf("The weather looks nice today in %v!", location)
